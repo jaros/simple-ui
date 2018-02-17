de810108f2224ab96d9cc05bdc1e127ca7dbc837
jaros <judin.jaroslav@gmail.com>
2018-02-17 21:26:15 +0100

    edit customer name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer} from  '../customer'
+
+@Component({
+  selector: "app-customers",
+  templateUrl: "./customers.component.html",
+  styleUrls: ["./customers.component.css"]
+})
+export class CustomersComponent implements OnInit {
+  
+  customer: Customer = {
+    id: 1,
+    name: "BillyBoy"
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
